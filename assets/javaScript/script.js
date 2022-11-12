7dbb4497590579905215123495a7aec6bf8f49f2
wyamet <wyattdomanski.md@gmail.com>
2022-11-11 16:03:00 -0800

    Got basic HTML and CSS started as well as created an index.html,css sheet and JS file

---
+++ assets/javaScript/script.js
@@ -0,0 +1,18 @@
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		// code will go here
+	}
+
+	function showResults(questions, quizContainer, resultsContainer){
+		// code will go here
+	}
+
+	// show the questions
+	showQuestions(questions, quizContainer);
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
